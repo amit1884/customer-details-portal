bbd5028459ae754c3de636a777ed33a05f7c8594
amit1884 <amitraj200raj@gmail.com>
2024-08-02 10:25:32 +0530

    removed spinner in photo section

--- src/Components/ImageContainer/ImageContainer.tsx
+++ src/Components/ImageContainer/ImageContainer.tsx
@@ -4,21 +4,17 @@ import Spinner from "../Spinner/Spinner";
 
 const ImageConatiner: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
   const fetchImages = async () => {
     try {
-      setLoading(true);
       const imageUrls: any = await Promise.all(
         Array.from({ length: 9 }, () =>
           fetch("https://picsum.photos/200").then((res) => res.url)
         )
       );
-      setLoading(false);
       setError("");
       setImages(imageUrls);
     } catch (error) {
-      setLoading(false);
       setError("Error fetching images");
       console.error("Error fetching images:", error);
     }
@@ -34,18 +30,13 @@ const ImageConatiner: React.FC = () => {
   }, []);
   return (
     <div className="photos-container">
-      {loading && !error && (
-        <div className="flex-center">
-          <Spinner />
-        </div>
-      )}
       {images?.length > 0 &&
         images.map((url, idx) => (
           <div className="thumbnail" key={idx}>
             <img src={url} alt={` ${idx + 1}`} />
           </div>
         ))}
-      {error && !loading && (
+      {error && (
         <div className="flex-center">
           <p className="error-txt">{error}</p>
         </div>
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import Users from "../../Components/Users/Users";
 const Home: React.FC = () => {
   return (
     <div className="app">
-      <div className="header">User Details Portal</div>
+      <div className="header">Customer Details Portal</div>
       <div className="container">
         <Users />
       </div>
